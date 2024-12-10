ffda3bb777963cee2e3742bd7c6459a4a1c3cc53
km-mainPC <kmatsumoto.fw@gmail.com>
2024-12-10 20:04:35 +0900

    added landing page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StackController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
 
 // Route::get('/dashboard', function () {
