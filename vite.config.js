a2e45b0360a323ccd17297e245463fd67a3cc363
km-mainPC <kmatsumoto.fw@gmail.com>
2024-12-27 21:12:48 +0900

    deployed (for the first time!)

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'rsources/js/main.js'
+                'resources/js/main.js'
             ],
             refresh: true,
         }),
