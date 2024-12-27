df40940ba1bbb1e49ea8a2730bcd15cf6acec11b
km-mainPC <kmatsumoto.fw@gmail.com>
2024-12-27 21:25:50 +0900

    disabled two-factord authentication temporarily

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', [StackController::class, 'list'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [StackController::class, 'list'])->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
